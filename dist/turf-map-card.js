4947c027caa7a2685cc169d85a3598038b7d90b3
Johannes Hansson <johannes.hansson@husqvarnagroup.com>
2024-12-15 15:27:09 +0100

    Adding changes to test how it updates

--- dist/turf-map-card.js
+++ dist/turf-map-card.js
@@ -3,12 +3,12 @@ class TurfMapCard extends HTMLElement {
     // Whenever the state changes, a new `hass` object is set. Use this to
     // update your content.
     set hass(hass) {
+      
       // Initialize the content if it's not there yet.
       if (!this.content) {
         this.innerHTML = `
-          <ha-card header="Example-card">
+          <ha-card header="Turf-map">
             <div class="card-content">
-                TURF Map
             </div>
           </ha-card>
         `;
@@ -20,9 +20,8 @@ class TurfMapCard extends HTMLElement {
       const stateStr = state ? state.state : "unavailable";
   
       this.content.innerHTML = `
-        The state of ${entityId} is ${stateStr}!
+        Turf Map source ${entityId} data: ${stateStr}
         <br><br>
-        <img src="http://via.placeholder.com/350x150">
       `;
     }
   
